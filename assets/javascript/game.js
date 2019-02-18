3f4c41bcd4c8b3ac39bcc57d61859138461a724f
team awesome <maxj@email.arizona.edu>
2019-02-18 10:02:07 -0800

    minor html updates and began psuedocoding javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+//Player guesses a number 
+
+// Four crystals lined up in a row and serve as buttons, each crystal has a random, hidden value between 1-12
+
+// Player receieves a random number, computerGuess, between 19-120
+
