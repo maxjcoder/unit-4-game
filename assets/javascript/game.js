a22e33314c739e32d7b62068a0bd82c075728087
team awesome <maxj@email.arizona.edu>
2019-02-18 10:17:53 -0800

    more psuedocode and linked js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,23 @@
-//Player guesses a number 
+//Player guesses an answer with numbers instead of letters 
 
 // Four crystals lined up in a row and serve as buttons, each crystal has a random, hidden value between 1-12
 
-// Player receieves a random number, computerGuess, between 19-120
+// Player receieves a random number, randomNumber, between 19-120
 
+//When player clicks a crystal, it adds a specific amount of points to the player's total score
+    //hide point amount until player clicks a gem
+    // upon click, update player score tally
+
+// Playe wins if tally matches computer guess from beginning of game
+
+// Player loses if tally goes above the random number
+
+// Game restarts when player wins or loses
+
+// When game restarts, player should be given a new random number 
+
+// When game restarts the gems should have new, hidden values 
+
+// When the game restarts the users score and score counter should reset to zero
+
+// Game should show the number of games the player wins and loses; refresh is not needed to restart the game 
