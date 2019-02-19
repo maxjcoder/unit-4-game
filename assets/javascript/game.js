64d56dcdc42c33f6a2c461796437aef5a17acecd
team awesome <maxj@email.arizona.edu>
2019-02-19 16:26:01 -0600

    text update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,9 @@ $("#loss").text(loss);
 function reset() {
     random = Math.floor(Math.random()*102+19);
     console.log(random);
+
     $("#randomnumber").text(random);
+    
     var gem1 = Math.floor(Math.random()*12+1);
     var gem2 = Math.floor(Math.random()*12+1);
     var gem3 = Math.floor(Math.random()*12+1);
