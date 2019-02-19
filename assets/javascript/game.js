a7007808a9517a70fb19ece09dac2081debdf9cd
team awesome <maxj@email.arizona.edu>
2019-02-18 21:23:45 -0800

    more html and styling; uniform gem images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,19 @@
-//Player guesses an answer with numbers instead of letters 
+$(document).ready(function() {
+
+    var random = Math.floor(Math.random()*102+19);
+
+    $("#numberToGet").text(random);
+
+}
+
+    //Player guesses an answer with numbers instead of letters 
 
 // Four crystals lined up in a row and serve as buttons, each crystal has a random, hidden value between 1-12
 
-// Player receieves a random number, randomNumber, between 19-120
+// Player receieves a random number, randomNumber, between 19-120 
+
+
+
 
 //When player clicks a crystal, it adds a specific amount of points to the player's total score
     //hide point amount until player clicks a gem
@@ -20,4 +31,5 @@
 
 // When the game restarts the users score and score counter should reset to zero
 
-// Game should show the number of games the player wins and loses; refresh is not needed to restart the game    
+// Game should show the number of games the player wins and loses; refresh is not needed to restart the game.
+
