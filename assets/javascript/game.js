d69f19f22a9345f94646353ad40663de8a88954e
team awesome <maxj@email.arizona.edu>
2019-02-22 19:07:53 -0700

    updates to logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,109 @@
 $(document).ready(function() {
 
-    var random = Math.floor(Math.random()*102+19);
+    var random = Math.floor(Math.random()*101+19);
 
     $("#randomNumber").text(random);
 
-    var gem1 = Math.floor(Math.random()*12+1);
-    var gem2 = Math.floor(Math.random()*12+1);
-    var gem3 = Math.floor(Math.random()*12+1);
-    var gem4 = Math.floor(Math.random()*12+1);
+    var gem1 = Math.floor(Math.random()*11+1);
+    var gem2 = Math.floor(Math.random()*11+1);
+    var gem3 = Math.floor(Math.random()*11+1);
+    var gem4 = Math.floor(Math.random()*11+1);
 
     var tally = 0;
     var win = 0;
     var loss = 0;
+    var yourScore = 0;
 
     $("#win").text(win);
     $("#loss").text(loss);
 
     function reset() {
-    random = Math.floor(Math.random()*102+19);
+    random = Math.floor(Math.random()*101+19);
     console.log(random);
 
     $("#randomnumber").text(random);
     
-    var gem1 = Math.floor(Math.random()*12+1);
-    var gem2 = Math.floor(Math.random()*12+1);
-    var gem3 = Math.floor(Math.random()*12+1);
-    var gem4 = Math.floor(Math.random()*12+1);
-    userTotal = 0;
+    var gem1 = Math.floor(Math.random()*11+1);
+    var gem2 = Math.floor(Math.random()*11+1);
+    var gem3 = Math.floor(Math.random()*11+1);
+    var gem4 = Math.floor(Math.random()*11+1);
+
+    yourScore = 0;
     $("#tally").text(yourScore);
 }
 
 function win() {
     alert("You Win!");
-    win++
+    win++;
     $("#win").text(win);
     reset();
 }
 
 function loss() {
     alert("You Lost!");
-    lose++
+    loss++;
     $("#loss").text(loss);
     reset();
 }
 
-$("#gem1").on("click", function() {
+$("#gem1").on ("click", function() {
     tally = yourScore + gem1;
     console.log("New Tally " + tally);
     $("#tally").text(yourScore);
 
-if (yourScore === randomNumber) {
-    win()
-}
-
-else if (yourScore > randomNumber) {
-    loss()
-}
+    if (yourScore == randomNumber) {
+        win()
+    }
+    
+    else if (yourScore > randomNumber) {
+        loss()
+    }
 
 })
 
+$("#gem2").on ("click", function() {
+    tally = yourScore + gem2;
+    console.log("New Tally" + tally);
+    $("#tally").text(yourScore);
+
+    if (yourScore == randomNumber) {
+        win()
+    }
+    
+    else if (yourScore > randomNumber) {
+        loss()
+    }
+    
+})
+
+$("#gem3").on("click", function() {
+    tally = yourScore + gem3;
+    console.log("New Tally" + tally);
+    $("#tally").text(yourScore);
+
+    if (yourScore == randomNumber) {
+        win()
+    }
+    
+    else if (yourScore > randomNumber) {
+        loss()
+    }
+})
+
+$("#gem4").on("click", function() {
+    tally = yourScore + gem4;
+    console.log("New Tally" + tally);
+    $("#tally").text(yourScore);
+
+    if (yourScore == randomNumber) {
+        win()
+    }
+    
+    else if (yourScore > randomNumber) {
+        loss()
+    }
+
+    });
 });
 
 
@@ -67,7 +112,6 @@ else if (yourScore > randomNumber) {
 // Four crystals lined up in a row and serve as buttons, each crystal has a random, hidden value between 1-12
 
 // Player receieves a random number, randomNumber, between 19-120 
-
 
 //When player clicks a crystal, it adds a specific amount of points to the player's total score
     //hide point amount until player clicks a gem
